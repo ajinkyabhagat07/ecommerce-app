728370f306d9a3ff3845f8bc65f62bab672c3c48
ajinkyabhagat07 <bhagatajinkya777@gmail.com>
2023-04-21 01:39:58 +0530

    first commit

---
+++ client/src/context/Search.js
@@ -0,0 +1,31 @@
+import { useState, useContext, createContext } from "react";
+
+
+const SearchContext = createContext();
+
+
+
+
+
+const SearchProvider = ({ children }) => {
+    const [auth, setAuth] = useState({
+        user: null,
+        keyword: "",
+        results: [],
+    });
+
+
+
+
+    return (
+        <SearchContext.Provider value={[auth, setAuth]}>
+            {children}
+        </SearchContext.Provider>
+    )
+}
+
+//custom hook
+
+const useSearch = () => useContext(SearchContext);
+
+export { useSearch, SearchProvider };
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+export const Policy = () => {
+    return (
+        <Layout title={'policy'}>
+            <div className="row contactus ">
+                <div className="col-md-6 ">
+                    <img
+                        src="/images/contactus.jpeg"
+                        alt="contactus"
+                        style={{ width: "100%" }}
+                    />
+                </div>
+
+            </div>
+        </Layout>
+    )
+}
